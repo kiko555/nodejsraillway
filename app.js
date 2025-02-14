bddc45c6090ca9259521e4c5a468d4b0a0701a50
kiko555 <kiko555@users.noreply.github.com>
2025-02-14 10:32:27 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,8 @@
 const express = require('express');
 const axios = require('axios');
-// const path = require('path');
-// const indexRouter = require('./routes/index');
 
 const app = express();
 const PORT = 3000;
-
-// // Serve static files from the "public" directory
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// // Use the router for handling routes
-// app.use('/', indexRouter);
-
-// // Catch-all route for handling 404 errors
-// app.use((req, res, next) => {
-//     res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-//   });
 
 // Middleware to parse JSON body
 app.use(express.json());
@@ -24,26 +11,28 @@ app.post('/OrderInitial.aspx', async (req, res) => {
     console.log('Received POST request:', req.body);
     
     const responseData = {
-        "strRsXML": "<?xml version='1.0' encoding='UTF-8'?><CUBXML><CAVALUE>b041e60ef25eec79733daf1864ac0a1c</CAVALUE><ORDERINFO><STOREID>250700008</STOREID><ORDERNUMBER>122A23083084</ORDERNUMBER><AMOUNT>201</AMOUNT></ORDERINFO><AUTHINFO><AUTHSTATUS>0000</AUTHSTATUS><AUTHCODE>773091</AUTHCODE><AUTHTIME>20250213230928</AUTHTIME><AUTHMSG>授權成功</AUTHMSG><CARDNO>559980******5107</CARDNO></AUTHINFO></CUBXML>",
-        "strRsJSON": null,
-        "strOrderInfo": "<?xml version='1.0' encoding='UTF-8'?><CUBXML><CAVALUE>eea326620371ca56d71bf4a65bef9e9e</CAVALUE><ORDERINFO><STOREID>250700008</STOREID><ORDERNUMBER>122A23083084</ORDERNUMBER></ORDERINFO></CUBXML>"
+        strRsXML: `<?xml version='1.0' encoding='UTF-8'?><CUBXML><CAVALUE>b041e60ef25eec79733daf1864ac0a1c</CAVALUE><ORDERINFO><STOREID>250700008</STOREID><ORDERNUMBER>122A23083084</ORDERNUMBER><AMOUNT>201</AMOUNT></ORDERINFO><AUTHINFO><AUTHSTATUS>0000</AUTHSTATUS><AUTHCODE>773091</AUTHCODE><AUTHTIME>20250213230928</AUTHTIME><AUTHMSG>授權成功</AUTHMSG><CARDNO>559980******5107</CARDNO></AUTHINFO></CUBXML>`,
+        strRsJSON: null,
+        strOrderInfo: `<?xml version='1.0' encoding='UTF-8'?><CUBXML><CAVALUE>eea326620371ca56d71bf4a65bef9e9e</CAVALUE><ORDERINFO><STOREID>250700008</STOREID><ORDERNUMBER>122A23083084</ORDERNUMBER></ORDERINFO></CUBXML>`
     };
     
     try {
-        // const callbackResponse = await axios.post('https://stage.comicrevive.com', responseData, {
-        //     headers: {
-        //         'Content-Type': 'application/json'
-        //     }
-        // });
-        // console.log('Callback response:', callbackResponse.data);
+        const callbackResponse = await axios.post('https://stage.comicrevive.com', responseData, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        console.log('Callback response:', callbackResponse.data);
         
         // Redirect using a POST request
         res.setHeader('Content-Type', 'text/html');
         res.send(`
             <html>
                 <body onload="document.forms[0].submit()">
-                    <form method="POST" action="https://stage.comicrevive.com/line_cathaypay_callback">
-                        <input type="hidden" name="data" value='${JSON.stringify(responseData)}'>
+                    <form method="POST" action="https://stage.comicrevive.com">
+                        <input type="hidden" name="strRsXML" value='${responseData.strRsXML.replace(/'/g, '&apos;')}'>
+                        <input type="hidden" name="strRsJSON" value='${responseData.strRsJSON}'>
+                        <input type="hidden" name="strOrderInfo" value='${responseData.strOrderInfo.replace(/'/g, '&apos;')}'>
                     </form>
                 </body>
             </html>
